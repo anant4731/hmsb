f966e5ba28db8cebe876b26faa7eca9a7b4d08f3
Anant Chauhan <anantchauhan4731@gmail.com>
2024-06-06 17:54:11 +0530

    error fix for deployment try-6

--- src/app/dashboard/add-admin/page.jsx
+++ src/app/dashboard/add-admin/page.jsx
@@ -1,73 +1,7 @@
-"use client";
-
-import { useState } from "react";
-import classes from "./addAdmin.module.css";
-const { Box, Button } = require("@mui/material");
+import AddAdmin from "@/components/AddAdmin/AddAdmin";
 
 const addAdminPage = () => {
-  const [username, setUsername] = useState("");
-  console.log("hello");
-  const [password, setPassword] = useState("");
-  const submitFormHandler = async (e) => {
-    e.preventDefault();
-    console.log(e);
-    const res = await fetch("/api/create-admin", {
-      method: "POST",
-
-      headers: {
-        "Content-Type": "application/json",
-      },
-
-      body: JSON.stringify({ body: { username, password } }),
-    });
-    const data = await res.json();
-    console.log(data);
-  };
-  return (
-    <div className={classes.container}>
-      <Box sx={{ width: "500px" }}>
-        <h2>ADD AN ADMIN</h2>
-        <div className={classes.single_input__div}>
-          <div className={classes.input__div}>
-            <label htmlFor="username">USERNAME : </label>
-            <input
-              type="text"
-              onChange={(e) => setUsername(e.target.value)}
-              name="username"
-              id="username"
-            />
-          </div>
-          <div className={classes.input__div}>
-            <label htmlFor="password">PASSWORD : </label>
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              id="password"
-              type="password"
-              name="passoword"
-            />
-          </div>
-          <Box
-            sx={{
-              width: "100%",
-              display: "flex",
-              justifyContent: "center",
-              marginTop: "20px",
-            }}
-          >
-            <Button
-              type="button"
-              variant="contained"
-              sx={{ width: "50%", height: "50px", fontSize: "25px" }}
-              onClick={submitFormHandler}
-              className={classes.save}
-            >
-              ADD
-            </Button>
-          </Box>
-        </div>
-      </Box>
-    </div>
-  );
+  return <AddAdmin />;
 };
 
 export default addAdminPage;
